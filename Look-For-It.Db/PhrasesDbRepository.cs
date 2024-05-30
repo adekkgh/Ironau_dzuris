de7957d52fb4cdf8172f1603ba0f6df645b1bf3c
GioevArsen <gioev.arsen15@list.ru>
2024-05-30 19:49:37 +0300

    endless game mode with changed ui

--- Look-For-It.Db/PhrasesDbRepository.cs
+++ Look-For-It.Db/PhrasesDbRepository.cs
@@ -20,6 +20,38 @@ namespace Look_For_It.Db
             return databaseContext.Phrases.ToList();
         }
 
+        public Phrase GetRandomPhrase()
+        {
+            Random random = new Random();
+            int rndIndex = random.Next(0, databaseContext.Phrases.Count());
+            return databaseContext.Phrases.ToList()[rndIndex];
+        }
+
+        public List<Phrase> GetWrongPhrases(Guid right_id)
+        {
+            Random random = new Random();
+            Phrase wrong_phrase_1;
+            Phrase wrong_phrase_2;
+
+            while (true)
+            {
+                int rndIndex_1 = random.Next(0, databaseContext.Phrases.Count());
+                int rndIndex_2 = random.Next(0, databaseContext.Phrases.Count());
+                wrong_phrase_1 = databaseContext.Phrases.ToList()[rndIndex_1];
+                wrong_phrase_2 = databaseContext.Phrases.ToList()[rndIndex_2];
+                if ( (wrong_phrase_1.Id != wrong_phrase_2.Id) && (wrong_phrase_1.Id != right_id) && (wrong_phrase_2.Id != right_id) )
+                {
+                    break;
+                }
+            }
+
+            return new List<Phrase>
+            {
+                wrong_phrase_1,
+                wrong_phrase_2
+            };
+        }
+
         public void Add(Phrase phrase)
         {
             databaseContext.Phrases.Add(phrase);
@@ -31,5 +63,7 @@ namespace Look_For_It.Db
     {
         public List<Phrase> GetPhrases();
         public void Add(Phrase phrase);
+        public Phrase GetRandomPhrase();
+        public List<Phrase> GetWrongPhrases(Guid right_id);
     }
 }
