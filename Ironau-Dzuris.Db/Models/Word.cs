28deaf93ca6c3bfa453db14945dc809e9d2762b4
GioevArsen <gioev.arsen15@list.ru>
2024-06-13 02:58:17 +0300

    feat/added new game mode, added new model - word, added words table to db, multiple ui additions

---
+++ Ironau-Dzuris.Db/Models/Word.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ironau_Dzuris.Db.Models
+{
+    public class Word
+    {
+        public Guid Id { get; set; }
+        public string Theme { get; set; }
+        public string Word_ru { get; set; }
+        public string Word_os { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
